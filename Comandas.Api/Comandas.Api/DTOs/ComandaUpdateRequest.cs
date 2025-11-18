57eecd2b3049933880330137e2bd377846d8ddb2
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-11-18 16:24:33 -0300

    Nova class dentro da pasta DTOs

---
+++ Comandas.Api/Comandas.Api/DTOs/ComandaUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Comandas.Api.DTOs;
+
+public class ComandaUpdateRequest
+{
+    public string NomeCliente { get; set; } = default!;
+    public int NumeroMesa { get; set; }
+    public ComandaItemUpdateRequest[] Itens { get; set; } = []; // lista
+}
+
+public class ComandaItemUpdateRequest
+{
+    public int Id { get; set; } // id da comanda item
+    public bool Remove { get; set; } // indicar se esta removendo
+    public int CardapioItemId { get; set; } // indicar se estar inserindo
+}
