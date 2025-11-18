7723c757c5198612c653a8691e1bc6637240126e
Millena Kappaun <164417128+millena-kppn@users.noreply.github.com>
2025-11-18 17:08:58 -0300

    Identificando chaves, add comentários

--- Comandas.Api/Comandas.Api/Models/Usuario.cs
+++ Comandas.Api/Comandas.Api/Models/Usuario.cs
@@ -5,8 +5,8 @@ namespace Comandas.Api.Models
 {
     public class Usuario
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]//Primary Key
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//Primary Key
         public int Id { get; set; }
         public string Nome { get; set; } = default!;
         public string Email { get; set; } = default!;
